0de3fb5ad2d0c96248491f98c0663422c95f800f
Dedipya Goswami <111869712+dedipya001@users.noreply.github.com>
2024-06-14 09:57:22 +0530

    Feed Page Structure Updated

---
+++ Frontend/src/components/FeedPage/FeedNavbar/FeedNavbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './FeedNavbar.css'
+const FeedNavbar = () => {
+  return (
+    <div className='NavBar'>
+      <div className='logo'>
+      </div>
+
+      <div className='siteName'>
+        </div>
+
+    <div className='navLinks'>
+    </div>
+
+    </div>
+  )
+}
+
+export default FeedNavbar
