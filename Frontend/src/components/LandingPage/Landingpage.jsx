dc97d98322e0510bdaa03ff181fe59727b1ac92b
Dedipya Goswami <111869712+dedipya001@users.noreply.github.com>
2024-06-19 15:38:47 +0530

    Changes in Landing Page

--- Frontend/src/components/LandingPage/Landingpage.jsx
+++ Frontend/src/components/LandingPage/Landingpage.jsx
@@ -1,13 +1,50 @@
+// // App.js
+// import React from 'react';
+// import { Parallax } from 'react-parallax';
+// import Header from './Header';
+// import HeroSection from './HeroSection';
+// import './LandingPage.css';
+
+// import Features from './FeaturesSection';
+
+// function LandingPage() {
+//   return (
+//     <div className="App2">
+//       <Header />
+//       <Parallax bgImage="/path/to/hero-background.jpg" strength={500}>
+//         <div style={{ height: '500px' }}> {/* Adjust height as needed */}
+//           <HeroSection />
+//         </div>
+//       </Parallax>
+//       <div className='SpacingDivision' style={{ marginBottom: "20%" }}></div>
+//       <Features />
+//       {/* <IntroductionSection />
+//       <AboutSection />
+//       <PricingSection />
+//       <TestimonialsSection />
+//       <Footer /> */}
+//     </div>
+//   );
+// }
+
+// export default LandingPage;
+
+
 // App.js
 import React from 'react';
+// import { Parallax } from 'react-parallax';
 import Header from './Header';
 import HeroSection from './HeroSection';
+import Features from './FeaturesSection';
+
 
 function LandingPage() {
   return (
     <div className="App2">
       <Header />
-      <HeroSection />
+          <HeroSection />
+      <div className='SpacingDivision' style={{ marginBottom: "50%" }}></div>
+      <Features />
       {/* <IntroductionSection />
       <AboutSection />
       <PricingSection />
@@ -18,3 +55,4 @@ function LandingPage() {
 }
 
 export default LandingPage;
+
